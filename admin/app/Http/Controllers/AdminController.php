e6b2c8fbaee1e5dbbb512ffb079f63213770a204
Farzad Rahman <farzadrahman59@gmail.com>
2018-03-14 12:58:41 +0600

    Farzad

--- admin/app/Http/Controllers/AdminController.php
+++ admin/app/Http/Controllers/AdminController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Auth;
+use Hash;
+use Session;
 
 class AdminController extends Controller
 {
@@ -13,7 +16,36 @@ class AdminController extends Controller
     }
 
     public function index(){
-      
-        return view('index');
+        $regiteredUser=10;
+        $visitors=10;
+        $message=5;
+        $subcriber=50;
+
+        return view('index')
+            ->with('regiteredUser',$regiteredUser)
+            ->with('visitors',$visitors)
+            ->with('subcriber',$subcriber)
+            ->with('message',$message);
+    }
+
+    public function settings(){
+
+        return view('settings');
+    }
+
+    public function changePass(Request $r){
+        $user=User::findOrFail(Auth::user()->userId);
+        $currentPass= Hash::make($r->oldPass);
+        $newPass=Hash::make($r->newPass);
+        if(Hash::check($r->oldPass, $user->password)){
+
+           $user->password=$newPass;
+           $user->save();
+
+            Session::flash('message', 'Password changes successfully');
+            return back();
+        }
+        Session::flash('message', 'Password did not match');
+        return back();
     }
 }
