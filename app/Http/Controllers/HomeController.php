84d0c5f071005983eeceaf0b86c1e3a6083c11f7
Farzad <farzadrahman59@gmail.com>
2018-04-10 00:02:06 +0600

    Farzad

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Resturant;
+use App\Purchase;
+use DB;
 
 class HomeController extends Controller
 {
@@ -11,8 +13,24 @@ class HomeController extends Controller
 
     public function index(){
 
+//        Top 6 sellers
+//        SELECT `restaurantId`,SUM(`total`) as totalcost FROM `purchase` GROUP BY `restaurantId` order BY totalcost desc LIMIT 6
+        $purchase=Purchase::select('purchase.restaurantId',DB::raw('SUM(purchase.total) as totalcost'))
+            ->groupBy('restaurantId')
+            ->orderBy('totalcost','desc')
+            ->limit(6)
+            ->get();
+        $resID=array();
+        foreach ($purchase as $p){
+            array_push($resID,$p->restaurantId);
+        }
+        $topRestaurants=Resturant::select('resturantId','name','image','address')->whereIn('resturantId',$resID)->get();
 
-        return view('welcome');
+
+//       End Top sellers
+
+        return view('welcome')
+            ->with('topRestaurants',$topRestaurants);
 
     }
 }
