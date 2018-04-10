7f7c0f9cc402e028e097e47f74e9b4e453ebad14
Farzad Rahman <farzadrahman59@gmail.com>
2018-04-10 18:06:33 +0600

    Farzad

--- db/findhalal.sql
+++ db/findhalal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2018 at 06:48 AM
+-- Generation Time: Apr 10, 2018 at 02:05 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
