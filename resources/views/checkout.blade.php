5d7b820a728748bf7e4d2f45164f2da705d5fcf3
Farzad Rahman <farzadrahman59@gmail.com>
2018-04-27 15:30:00 +0600

    Farzad

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -256,41 +256,10 @@
             }
         };
 
-        // Create an instance of the card Element.
-        var card = elements.create('card', {style: style});
 
-        // Add an instance of the card Element into the `card-element` <div>.
-        card.mount('#card-element');
-
-        // Handle real-time validation errors from the card Element.
-        card.addEventListener('change', function(event) {
-            var displayError = document.getElementById('card-errors');
-            if (event.error) {
-                displayError.textContent = event.error.message;
-            } else {
-                displayError.textContent = '';
-            }
-        });
-
-        // Handle form submission.
-        var form = document.getElementById('payment-form');
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-
-            stripe.createToken(card).then(function(result) {
-                if (result.error) {
-                    // Inform the user if there was an error.
-                    var errorElement = document.getElementById('card-errors');
-                    errorElement.textContent = result.error.message;
-                } else {
-                    // Send the token to your server.
-                    stripeTokenHandler(result.token);
-                }
-            });
-        });
 
         function stripeTokenHandler(token) {
-//        console.log(token);
+       // console.log(token);
             // Insert the token ID into the form so it gets submitted to the server
             var form = document.getElementById('payment-form');
             var hiddenInput = document.createElement('input');
@@ -300,7 +269,7 @@
             form.appendChild(hiddenInput);
 
             // Submit the form
-//            form.submit();
+            form.submit();
         }
     </script>
 
@@ -471,6 +440,39 @@
                     document.getElementById('PayNowCash').style.display = 'none';
 //                    document.getElementById('PayNowCard').style.display = 'block';
                     document.getElementById('demo').style.display = 'block';
+
+                    // Create an instance of the card Element.
+                    var card = elements.create('card', {style: style});
+
+                    // Add an instance of the card Element into the `card-element` <div>.
+                    card.mount('#card-element');
+
+                    // Handle real-time validation errors from the card Element.
+                    card.addEventListener('change', function(event) {
+                        var displayError = document.getElementById('card-errors');
+                        if (event.error) {
+                            displayError.textContent = event.error.message;
+                        } else {
+                            displayError.textContent = '';
+                        }
+                    });
+
+                    // Handle form submission.
+                    var form = document.getElementById('payment-form');
+                    form.addEventListener('submit', function(event) {
+                        event.preventDefault();
+
+                        stripe.createToken(card).then(function(result) {
+                            if (result.error) {
+                                // Inform the user if there was an error.
+                                var errorElement = document.getElementById('card-errors');
+                                errorElement.textContent = result.error.message;
+                            } else {
+                                // Send the token to your server.
+                                stripeTokenHandler(result.token);
+                            }
+                        });
+                    });
 
                 }
             });
