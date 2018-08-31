3970e83bb1ec7ffe5dd7c318d3b7fb5c64fe6e5d
Farzad <farzadrahman59@gmail.com>
2018-08-31 22:43:13 +0600

    problem in dom pdf report

---
+++ admin/resources/views/report/pdf.blade.php
@@ -0,0 +1,184 @@
+<html>
+
+<body>
+
+<table width="100%">
+    <tr>
+        <td width="30%"><img src="{{url('public/images/findhalal.png')}}" height="100px" width="200px"></td>
+        <td width="40%"></td>
+        <td>
+            <table width="70%" >
+                <tr  style="background-color: #DEEBF7" >
+                    <td width="40%"> <b>Unternehmenszentrale: </b><br>
+                        Mainzer Landstraße 49, 60329 Frankfurt am Main, DE<br>
+                        Phone: +4917686097107; Email: info@findhalal.de</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<table width="100%">
+    <tr height="50px"></tr>
+
+    <tr>
+        <td>Hallo <b>Mohammad Zahid Kamal,</b> </td>
+    </tr>
+    <tr height="20px"></tr>
+
+    <tr>
+        <td width="10%">vielen Dank fuer Ihre Bestellung bei <b>FindHalal.de.</b> Hier sind Ihre Bestellinformationen. </td>
+    </tr>
+
+</table>
+
+<table width="100%">
+    <tr width="100%" height="20px"></tr>
+
+    <tr   width="100%" style="background-color: #DEEBF7" >
+        <td style="padding: 10px"><b>Rechnung</b></td>
+    </tr>
+    <tr width="40%">
+        <table width="60%">
+            <tr>
+                <td>Ihr Name:</td>
+                <td>|</td>
+                <td>Mohammad Zahid Kamal</td>
+            </tr>
+            <tr>
+                <td>Lieferadresse:</td>
+                <td>|</td>
+                <td> Heddernheimer Landstraße 41, 60439 Frankfurt am Main, Deutschland</td>
+            </tr>
+            <tr>
+                <td>Bestellnummer:</td>
+                <td>|</td>
+                <td>FH1807140031</td>
+            </tr>
+            <tr>
+                <td>Bestelldatum:</td>
+                <td>|</td>
+                <td>14-July-2018 ; 12:51pm</td>
+            </tr>
+            <tr>
+                <td>Lieferart:</td>
+                <td>|</td>
+                <td>Hauslieferung</td>
+            </tr>
+
+
+        </table>
+
+    </tr>
+
+
+</table>
+
+<table width="100%">
+    <tr width="100%" height="20px"></tr>
+
+    <tr   width="100%" style="background-color: #DEEBF7" >
+        <td style="padding: 10px"><b>Verkaufsdetails</b></td>
+    </tr>
+    <tr width="100%" height="20px"></tr>
+    <tr width="40%">
+
+        <table width="100%">
+            <tr style="background-color: #DEEBF7">
+                <td width="5%">Sl.Nr</td>
+                <td width="15%">Datum</td>
+                <td width="15%">Bestellnummer</td>
+                <td width="15%">Bestellstatus </td>
+                <td width="8%">Bestellpreis (€)</td>
+                <td width="8%">Online bezahlt (€)</td>
+                <td width="15%">Bar bezahlt (€)</td>
+            </tr>
+
+            <tr >
+                <td width="5%">1</td>
+                <td width="15%">01-August-2018</td>
+                <td width="15%">FH1808010031 </td>
+                <td width="15%">Geliefert </td>
+                <td width="8%">19.00</td>
+                <td width="8%">-</td>
+                <td width="15%">19.00</td>
+            </tr>
+            <tr >
+                <td width="5%">1</td>
+                <td width="15%">01-August-2018</td>
+                <td width="15%">FH1808010031 </td>
+                <td width="15%">Geliefert </td>
+                <td width="8%">19.00</td>
+                <td width="8%">-</td>
+                <td width="15%">19.00</td>
+            </tr>
+
+
+
+
+
+        </table>
+
+    </tr>
+
+
+</table>
+
+
+<table width="100%">
+    <tr height="50px"></tr>
+
+    <tr>
+        <td>Für Rückfragen stehen wir Ihnen jederzeit gerne zur Verfügung. Sie erreichen uns wie folgt. </td>
+    </tr>
+    <tr height="20px"></tr>
+
+    <tr>
+        <td width="10%">Mit freundlichen Grüßen,<br>
+            Ihr Team von FindHalal.de </td>
+    </tr>
+
+</table>
+
+<table width="100%">
+    <tr height="50px"></tr>
+    <tr>
+        <td width="10%"><b>FindHalal</b> <br>
+            Mainzer Landstraße 49,<br>
+            60329 Frankfurt am Main, DE <br>
+            Telefon: +4917686097107 <br>
+            Email: info@findhalal.de<br>
+            URL: www.findhalal.de<br>
+        </td>
+    </tr>
+
+</table>
+<table width="100%">
+    <tr height="50px"></tr>
+
+    <tr align="right">
+        <td>** Alle Artikel Preise inkl Mw.-St. (7%) <br>
+            *** Diese Rechnung ist elektronisch erstellt und daher ohne Unterschrift gültig
+        </td>
+    </tr>
+
+
+</table>
+
+<table width="100%">
+    <tr height="10px"></tr>
+
+    <tr>
+        <td onclose="4"></td>
+        <td style="font-size: 12px; ">© FindHalal 2018. All rights reserved
+        </td>
+    </tr>
+
+
+</table>
+
+
+
+
+</body>
+</html>
