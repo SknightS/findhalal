ba81e920814095993fd803525fd1f7ee180c55c0
rumi <mujtaba.rumi1@gmail.com>
2018-10-01 13:49:30 +0600

    rumi

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -240,11 +240,12 @@ class RestaurantController extends Controller
                 //check customer zip to resturant zip
                 $zipCheck=ZipCode::where('fkresturantId',$resid)
                     ->where('zip',$r->zip)
-                    ->count();
-                if($zipCheck==0 && Session::get('ordertype') == "Delivery"){
+                    ->first();
+
+                if( !$zipCheck   && Session::get('ordertype') == "Delivery"){
                     $code= 'Zipcode Error';
                     $msg='Sorry we dont delivery to your zipcode';
-                    $data=array('cardError'=>'2','code'=>$code,'message'=>$msg);
+                    $data=array('ZipError'=>'3','code'=>$code,'message'=>$msg);
                     return $data;
                 }
 
